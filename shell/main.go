246f457d69e2090b473b11b9fd1f075dd5d26b7a
nathan <nathan-moore1@outlook.com>
2025-03-29 02:22:32 -0600

    Implemented shell

--- shell/main.go
+++ shell/main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"log"
 	"os"
 	"os/signal"
+	"os/user"
 	"strings"
 	"syscall"
 )
@@ -17,7 +19,32 @@ func main() {
 	}()
 
 	for {
-		print("# ")
+		getwd, err := os.Getwd()
+		if err != nil {
+			return
+		}
+		homedir, err := os.UserHomeDir()
+		if err != nil {
+			return
+		}
+
+		current, err := user.Current()
+		if err != nil {
+			return
+		}
+
+		hostname, err := os.Hostname()
+		if err != nil {
+			return
+		}
+
+		if strings.Contains(getwd, homedir) {
+			getwd = strings.Replace(getwd, homedir, "", 1)
+			fmt.Printf("[%s@%s:~%s]$ ", current.Name, hostname, getwd)
+		} else {
+			fmt.Printf("[%s@%s:%s]$ ", current.Name, hostname, getwd)
+		}
+
 		line := scanner()
 		lines := strings.Split(line, " ")
 		parse(lines)
